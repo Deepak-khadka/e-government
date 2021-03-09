338f69c41fc55cd736b2e2874622987c76e7e4f5
Deepak khadka <deepakkhadka@neputer.com>
2021-03-09 16:13:33 +0545

    Reactjs Init

--- resources/js/components/Example.js
+++ resources/js/components/Example.js
@@ -1,19 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import NavBar from "./includes/NavBar";
+import Home from "./Home";
 
 function Example() {
     return (
-        <div className="container">
-            <div className="row justify-content-center">
-                <div className="col-md-8">
-                    <div className="card">
-                        <div className="card-header">Example Component</div>
+        <>
+            <NavBar />
+            <Home/>
+        </>
 
-                        <div className="card-body">I'm an example component!</div>
-                    </div>
-                </div>
-            </div>
-        </div>
     );
 }
 
--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -5,11 +5,10 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-    <title>Home Page</title>
+    <title>Document</title>
 </head>
 <body>
-    <div id="example"></div>
+    <div id="profile"></div>
 </body>
 <script src="{{ asset('js/app.js') }}" defer></script>
 
